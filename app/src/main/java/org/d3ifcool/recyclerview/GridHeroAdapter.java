183bf4370b519742fc6df315a8e7837da6d236d1
Danis Zaidan <daniszaidan@gmail.com>
2019-10-15 16:46:58 +0700

    Recycler View - Grid Version

---
+++ app/src/main/java/org/d3ifcool/recyclerview/GridHeroAdapter.java
@@ -0,0 +1,52 @@
+package org.d3ifcool.recyclerview;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+
+import java.util.ArrayList;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class GridHeroAdapter extends RecyclerView.Adapter<GridHeroAdapter.GridViewHolder>{
+
+    private ArrayList<Hero> listHero;
+
+    public GridHeroAdapter(ArrayList<Hero> listHero) {
+        this.listHero = listHero;
+    }
+
+    @NonNull
+    @Override
+    public GridViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_grid_hero, parent, false);
+        return new GridViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull GridViewHolder holder, int position) {
+        Hero hero = listHero.get(position);
+        Glide.with(holder.itemView.getContext())
+                .load(hero.getPhoto())
+                .apply(new RequestOptions().override(55, 55))
+                .into(holder.imgPhoto);
+    }
+
+    @Override
+    public int getItemCount() { return listHero.size(); }
+
+    public class GridViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView imgPhoto;
+
+        public GridViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imgPhoto = itemView.findViewById(R.id.img_item_photo);
+        }
+    }
+}
